ec9e66dcc177730e913b5975c943a6e9a4afc38d
Eber Alejo <eber4202@hotmail.com>
2023-02-26 18:29:40 -0600

    ADD: bookmar button and toggle count button

--- components/ToggleCountButton.tsx
+++ components/ToggleCountButton.tsx
@@ -1,31 +1,40 @@
-import { ReactNode, useState } from 'react'
+import Image from 'next/image'
+import { useState, useRef } from 'react'
 
 type CountButtonProps = {
   initialCount: number
-  children: ReactNode
+  srcIcon: string,
+  srcActiveIcon: string,
+  iconWidth?: number,
+  iconHeigth?: number
 }
 
 let IS_COUNT = false
 
-export default function ToggleCountButton ({ initialCount, children }: CountButtonProps) {
+export default function ToggleCountButton ({ initialCount, srcIcon, srcActiveIcon, iconWidth = 40, iconHeigth = 40 }: CountButtonProps) {
   const [count, setCount] = useState(initialCount)
+  const imageRef = useRef<HTMLImageElement>(null)
 
   const toggleCount = () => {
     if (!IS_COUNT) {
+      imageRef.current!.classList.add('animate-likeVideo')
+      imageRef.current!.src = srcActiveIcon
       setCount(count + 1)
       IS_COUNT = true
       return
     }
 
     if (IS_COUNT) {
+      imageRef.current!.classList.remove('animate-likeVideo')
+      imageRef.current!.src = srcIcon
       setCount(count - 1)
       IS_COUNT = false
     }
   }
 
   return (
-    <button onClick={() => toggleCount()}>
-        {children}
+    <button onClick={() => toggleCount()} >
+        <Image ref={imageRef} src={srcIcon} width={iconWidth} height={iconHeigth} alt="A simple icon element" />
         <p>{count}</p>
     </button>
   )
--- components/videoControls/BookMarkControl.tsx
+++ components/videoControls/BookMarkControl.tsx
@@ -1,10 +1,7 @@
-import Image from 'next/image'
 import ToggleCountButton from '../ToggleCountButton'
 
 export default function BookMarkControl () {
   return (
-    <ToggleCountButton initialCount={254}>
-      <Image src='/bookmark.svg' alt='book mark' width={40} height={40} />
-    </ToggleCountButton>
+    <ToggleCountButton initialCount={254} srcIcon='/bookmark.svg' srcActiveIcon='/bookmark-yellow.svg' />
   )
 }
