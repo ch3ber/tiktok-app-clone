d4b883b8bac3754c50200bf396cf7ff548ff4285
Eber Alejo <eber4202@hotmail.com>
2023-02-26 19:33:06 -0600

    ADD: env config

---
+++ config.ts
@@ -0,0 +1,5 @@
+export const config = {
+  API_URL: process.env.API_URL,
+  X_RapidAPI_Key: process.env.X_RapidAPI_Key,
+  X_RapidAPI_Host: process.env.X_RapidAPI_Host
+}
--- next.config.js
+++ next.config.js
@@ -3,6 +3,11 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ['p16-sign-va.tiktokcdn.com', 'picsum.photos']
+  },
+  env: {
+    API_URL: process.env.API_URL,
+    X_RapidAPI_Key: process.env.X_RapidAPI_Key,
+    X_RapidAPI_Host: process.env.X_RapidAPI_Host
   }
 }
 
