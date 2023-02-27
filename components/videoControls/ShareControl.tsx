2c2ee7be2d12361badbc8111fcbfe3771946fa8a
Eber Alejo <eber4202@hotmail.com>
2023-02-26 18:29:48 -0600

    ADD: share control

--- components/videoControls/ShareControl.tsx
+++ components/videoControls/ShareControl.tsx
@@ -1,10 +1,7 @@
 import Image from 'next/image'
-import ToggleCountButton from '../ToggleCountButton'
 
 export default function ShareControl () {
   return (
-    <ToggleCountButton initialCount={254}>
-      <Image src='/share-arrow.svg' alt='book mark' width={40} height={40} />
-    </ToggleCountButton>
+    <Image src='/share-arrow.svg' alt='book mark' width={40} height={40} />
   )
 }
