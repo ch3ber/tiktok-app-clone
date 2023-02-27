d45008381087c0c11b97a91d8a6be18f521316c7
Eber Alejo <eber4202@hotmail.com>
2023-02-26 20:45:00 -0600

    FIX: ignore never type error

--- components/VideoList.tsx
+++ components/VideoList.tsx
@@ -17,6 +17,7 @@ export default function VideoList () {
     <section className='pb-14 h-full absolute top-0 right-0 left-0 flex flex-col snap-y snap-mandatory overflow-y-auto'>
       {!videos
         ? 'Loading...'
+        // @ts-ignore
         : videos.map((video, index) => {
           return <Video key={index} src={video.video.download_addr.url_list[0]} />
         })
